11549431894bfba1eed46e5db53c5e4eb4537d81
sheltonlain <123353186+sheltonlain@users.noreply.github.com>
2023-01-23 01:55:18 -0600

    Upload Homework1

---
+++ Homework1.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework1;
+class Homework1
+{
+    static void Main(string[] args)
+    {
+        double x, y, z;
+
+        x=2.5;
+        y=3.3;
+        z=4*x*x+3*y;
+
+        Console.WriteLine("X = "+x+", Y = "+y);
+        Console.WriteLine("The value of Z is: "+z);
+        
+
+    }
+}
